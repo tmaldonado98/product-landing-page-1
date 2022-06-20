23553f627d5eeeb01b7764ed89a8024e2e5920ae
tomas <tmaldonadotrs@gmail.com>
2022-06-20 16:46:49 -0700

    JS functions made for toggle on-off for mobile menu. Hover effects turned off for images. Service section card widths adjusted for mobile, pending tablet run.

--- main.js
+++ main.js
@@ -1,16 +1,44 @@
 /*document.getElementById('menu').setAttribute('style', 'visibility:hidden');
 document.getElementById('menu').setAttribute('style', 'visibility:visible');*/
 
+    let menu = document.getElementById('menu');
 
 function showMenu () {
-    let menu = document.getElementById('menu');
-    if (menu.style.display === "none") {
+    if (menu.style.display != "block") {
       menu.style.display = "block";
     } else {
       menu.style.display = "none";
     }
-    
 };
+
+    let contact1 = document.getElementById('contact-item-mob1');
+    let contact2 = document.getElementById('contact-item-mob2');
+    let contact3 = document.getElementById('contact-item-mob3');
+
+ function showMenu2 () {
+
+      if (contact1.style.display != "block") {
+      contact1.style.display = "block";
+    } else {
+      contact1.style.display = "none";
+    };  
+    
+    if (contact2.style.display != "block") {
+      contact2.style.display = "block";
+    } else {
+      contact2.style.display = "none";
+    };  
+
+    if (contact3.style.display != "block") {
+      contact3.style.display = "block";
+    } else {
+      contact3.style.display = "none";
+    }  
+  };
+
+
+
+
 /*
 function clickMenu () {
     menu.addEventListener('click', showMenu())
